44890f42643feab5aab60bdd1cc634fca5ff49d0
SrijonSarker <138516129+SrijonSarker@users.noreply.github.com>
2024-03-19 23:24:54 +0600

    navbar created

---
+++ src/Components/MatchesContainer/MatchesContainer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MatchesContainer = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default MatchesContainer;
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -4,7 +4,7 @@ import { Outlet } from 'react-router-dom';
 
 const Home = () => {
     return (
-        <div>
+        <div className='p-6'>
         <Navbar></Navbar>
         <Outlet></Outlet>
         </div>
